18c9869d670f2a61a4813ed6dc0ab20183a84ee5
Arsens <arsens.morins@chi.lv>
2019-04-06 08:59:24 +0300

    runner

--- src/test/java/AndroidAppium.java
+++ src/test/java/AndroidAppium.java
@@ -11,28 +11,17 @@ import java.util.concurrent.TimeUnit;
 
 public class AndroidAppium {
 
-   AppiumDriver driver;
+   WebDriver driver;
 
     @Before
     public void setUp() throws MalformedURLException {
-        // Created object of DesiredCapabilities class.
         DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability("platformName", "Android");
         cap.setCapability("deviceName", "Galaxy S7");
         cap.setCapability("app", "/Users/arsensmorins/Downloads/ApiDemos-debug.apk");
-//        cap.setCapability("appPackage", "io.appium.android.apis");
-//        cap.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-//        driver = new AndroidDriver(cap);
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
-
-//    @Test
-//    public void firstTest() {
-////        driver.findElement(By.id("Accessibility")).click();
-//        WebElement access = driver.findElement(By.id("Accessibility"));
-//        access.click();
-//    }
 }
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -3,12 +3,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class Runner extends AndroidAppium{
+public class Runner extends AndroidAppium {
 
     @Test
     public void firstTest() {
-//        driver.findElement(By.id("Accessibility")).click();
-        WebElement access = driver.findElement(By.id("Accessibility"));
-        access.click();
+        driver.findElement(By.id("Accessibility")).click();
+//        WebElement access = driver.findElement(By.id("Accessibility"));
+//        access.click();
+//    }
     }
 }
